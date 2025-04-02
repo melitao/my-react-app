427bca06d4ba4d620c7348a03de29053e32009c7
Melita Ocampo <melita@wustl.edu>
2025-04-02 12:33:01 -0500

    Added square component

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,23 +1,30 @@
+function Square() {
+  return <button className="square">1</button>;
+}
+
+
+
 //creates a component - a piece of reusable code that represents a part of a user interface
 //defines a function called Square
 export default function Board() { //export makes this function accessible outside of this file, default tells other files using your code that it’s the main function in your file
   return (
     <>
       <div className="board-row">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
       <div className="board-row">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square />
+        <Square />
+        <Square />
       </div>
     </>
   );
 }
+
