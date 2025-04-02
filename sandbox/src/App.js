cb788efd65c66572044b0e748ee689bb203e096a
Melita Ocampo <melita@wustl.edu>
2025-04-02 05:09:48 -0500

    Added another X button.

--- sandbox/src/App.js
+++ sandbox/src/App.js
@@ -1,5 +1,10 @@
 //creates a component - a piece of reusable code that represents a part of a user interface
 //defines a function called Square
 export default function Square() { //export makes this function accessible outside of this file, default tells other files using your code that it’s the main function in your file
-  return <button className="square">X</button>; //returns a button, <button> is a JSX element (combo of JS and HTML), className="square" is a button property that tells CSS how to style
+  return (
+    <>
+      <button className="square">X</button>
+      <button className="square">X</button>
+    </>
+  );
 }
